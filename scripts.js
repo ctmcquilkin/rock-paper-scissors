7a7a61e7b055c95314c9b251bf26043dbbf91c31
ctmcquilkin <ctmcquilkin@gmail.com>
2024-03-30 21:53:12 -0500

    changed console log statements to DOM methods. ToDo: This version appends new statements for each round but the game should re-use the same text elements.

--- scripts.js
+++ scripts.js
@@ -5,6 +5,7 @@ let playerWins = 0;
 let computerWins = 0;
 
 const buttons = document.querySelectorAll('button');
+const container = document.querySelector('#info'); 
 
 // we use the .forEach method to iterate through each button
 buttons.forEach((button) => {
@@ -27,46 +28,69 @@ function getComputerChoice() {
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
     computerSelection = getComputerChoice();
-    console.log('Player Selection: ' + playerSelection + " Computer Selection: " + computerSelection);
+    let selections = document.createElement('p');
+    let winner = document.createElement('p');
+    selections.textContent = 'Player Selection: ' + playerSelection + " Computer Selection: " + computerSelection;
+    container.appendChild(selections);
+    container.appendChild(winner);
     switch(playerSelection) {
         case 'rock':
             if ( computerSelection === 'scissors' ) {
-                console.log('You won! Rock beats Scissors');
+                winner.innerHTML = 'You won! Rock beats Scissors';
                 playerWins++;
                 return 'You won! Rock beats Scissors';
             } else if ( computerSelection === 'paper' ) {
-                console.log("You lost. Paper beats Rock");
+                winner.innerHTML = "You lost. Paper beats Rock";
                 computerWins++;
                 return "You lost. Paper beats Rock";
             } else if (computerSelection === 'rock' ) {
-                console.log("It's a Tie!");
+                winner.innerHTML = "It's a Tie!";
                 return "It's a Tie!";
             }
         case 'paper':
             if ( computerSelection === 'rock' ) {
-                console.log('You won! Paper beats Rock');
+                winner.innerHTML = 'You won! Paper beats Rock';
                 playerWins++;
                 return 'You won! Paper beats Rock';
             } else if ( computerSelection === 'scissors' ) {
-                console.log("You lost. Scissors beats Paper");
+                winner.innerHTML = "You lost. Scissors beats Paper";
                 computerWins++;
                 return "You lost. Scissors beats Paper";
             } else if (computerSelection === 'paper' ) {
-                console.log("It's a Tie!");
+                winner.innerHTML = "It's a Tie!";
                 return "It's a Tie!";
             }
         case 'scissors':
             if ( computerSelection === 'paper' ) {
-                console.log('You won! Scissors beats Paper');
+                winner.innerHTML = 'You won! Scissors beats Paper';
                 playerWins++;
                 return 'You won! Scissors beats Paper';
             } else if ( computerSelection === 'rock' ) {
-                console.log("You lost. Rock beats Scissors");
+                winner.innerHTML = "You lost. Rock beats Scissors";
                 computerWins++;
                 return "You lost. Rock beats Scissors";
             } else if (computerSelection === 'scissors' ) {
-                console.log("It's a Tie!");
+                winner.innerHTML = "It's a Tie!";
                 return "It's a Tie!";
             }
     }
   }
+
+//   function playGame() {
+//     let winner = '';
+//     let score = document.createElement('p');
+//     for ( let i = 1; i < 6; i++ ) {
+//         // score.textContent = "Rock Paper Sissors! Round: " + i + "You: " + playerWins + "Computer: " + computerWins;
+//         // container.appendChild(score);
+//         playRound(playerSelection, computerSelection);
+        
+//     }
+//     if ( playerWins > computerWins ) {
+//         winner = 'You';
+//     } else {
+//         winner = 'The computer';
+//     }
+//     // score.textContent = 'Game over. ' + winner + ' won.';
+//     // container.appendChild(score);
+// }
+// playGame()
