cd86e11468372bfed31c58e171c4500fc1119ea4
ctmcquilkin <ctmcquilkin@gmail.com>
2024-04-02 22:09:29 -0500

    fixed bug in getComputerChoice, re-wrote the playGame function to keep score and show the end state, adjusted the playRound function to clear the results from the last game on the end state.

--- scripts.js
+++ scripts.js
@@ -3,12 +3,17 @@ let computerSelection = '';
 let playerSelection = '';
 let playerWins = 0;
 let computerWins = 0;
+let round = 1;
 
+let score = document.createElement('p');
 const buttons = document.querySelectorAll('button');
 const container = document.querySelector('#info'); 
 
 let winner = document.createElement('p');
 let selections = document.createElement('p');
+
+score.textContent = 'Round #: ' + round + ' Computer: ' + computerWins + ' You: ' + playerWins;
+container.appendChild(score);
 
 // we use the .forEach method to iterate through each button
 buttons.forEach((button) => {
@@ -22,76 +27,89 @@ buttons.forEach((button) => {
 });
 
 function getComputerChoice() {
-    function getRandomInt(max) {
-        return Math.floor(Math.random() * max);
-    }
-    return computerSelection = CHOICES[getRandomInt(1, 3)];
+    function randomIntFromInterval(min, max) { // min and max included 
+        return Math.floor(Math.random() * (max - min + 1) + min)
+      }
+      
+      const rndInt = randomIntFromInterval(0, 2)
+      return computerSelection = CHOICES[rndInt];
 }
 
 function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
-    computerSelection = getComputerChoice();
-    selections.textContent = 'Player Selection: ' + playerSelection + " Computer Selection: " + computerSelection;
+    selections.textContent = 'You selected: ' + playerSelection + " The Computer selected: " + computerSelection;
     container.appendChild(selections);
     container.appendChild(winner);
+    if (round === 5) {
+        winner.innerHTML = '';
+        playGame();
+        return;
+    };
     switch(playerSelection) {
         case 'rock':
             if ( computerSelection === 'scissors' ) {
                 winner.innerHTML = 'You won! Rock beats Scissors';
                 playerWins++;
+                playGame();
                 return 'You won! Rock beats Scissors';
             } else if ( computerSelection === 'paper' ) {
                 winner.innerHTML = "You lost. Paper beats Rock";
                 computerWins++;
+                playGame();
                 return "You lost. Paper beats Rock";
             } else if (computerSelection === 'rock' ) {
                 winner.innerHTML = "It's a Tie!";
+                playGame();
                 return "It's a Tie!";
             }
         case 'paper':
             if ( computerSelection === 'rock' ) {
                 winner.innerHTML = 'You won! Paper beats Rock';
                 playerWins++;
+                playGame();
                 return 'You won! Paper beats Rock';
             } else if ( computerSelection === 'scissors' ) {
                 winner.innerHTML = "You lost. Scissors beats Paper";
                 computerWins++;
+                playGame();
                 return "You lost. Scissors beats Paper";
             } else if (computerSelection === 'paper' ) {
                 winner.innerHTML = "It's a Tie!";
+                playGame();
                 return "It's a Tie!";
             }
         case 'scissors':
             if ( computerSelection === 'paper' ) {
                 winner.innerHTML = 'You won! Scissors beats Paper';
                 playerWins++;
+                playGame();
                 return 'You won! Scissors beats Paper';
             } else if ( computerSelection === 'rock' ) {
                 winner.innerHTML = "You lost. Rock beats Scissors";
                 computerWins++;
+                playGame();
                 return "You lost. Rock beats Scissors";
             } else if (computerSelection === 'scissors' ) {
                 winner.innerHTML = "It's a Tie!";
+                playGame();
                 return "It's a Tie!";
             }
     }
   }
 
-//   function playGame() {
-//     let winner = '';
-//     let score = document.createElement('p');
-//     for ( let i = 1; i < 6; i++ ) {
-//         // score.textContent = "Rock Paper Sissors! Round: " + i + "You: " + playerWins + "Computer: " + computerWins;
-//         // container.appendChild(score);
-//         playRound(playerSelection, computerSelection);
-        
-//     }
-//     if ( playerWins > computerWins ) {
-//         winner = 'You';
-//     } else {
-//         winner = 'The computer';
-//     }
-//     // score.textContent = 'Game over. ' + winner + ' won.';
-//     // container.appendChild(score);
-// }
-// playGame()
+function playGame() {
+    let winner = '';
+    round++;
+    score.textContent = 'Round #: ' + (round - 1) + ' Computer wins: ' + computerWins + ' Your wins: ' + playerWins;
+
+    if (round === 6) {
+        selections.textContent = 'Game over'
+        round = 1;
+        if (playerWins > computerWins) {
+            score.textContent = 'You won the game.' + ' Computer: ' + computerWins + ' You: ' + playerWins;
+            winner.textContent = '';
+        } else {
+            score.textContent = 'The computer won the game.' + ' Computer: ' + computerWins + ' You: ' + playerWins;
+            winner.textContent = '';
+        };
+    };
+};
